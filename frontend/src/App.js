a582e6d73f4b8ae09e417e66099b30fbb547cb48
Nikhil Patel <Nikhilpatel2017@yahoo.comgit config --global user.email Nikhilpatel2017@yahoo.com>
2021-03-08 23:18:02 +0530

    second commit completed front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
-
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Header from './components/header';
+import HomeScreen from './screens/homeScreen';
+import Footer from './components/footer';
+import ProductScreen from './screens/productScreen';
 
 function App() {
   return (
     <>
-      <h1> Welcome to Proshop </h1>
+      <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+            <Route exact path="/" component={HomeScreen} />
+            <Route path="/product/:_id" component={ProductScreen} />
+        </Container>
+      </main>      
+      <Footer />
+      </Router>
     </>
   );
 }
